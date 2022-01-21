3284e88185b77708efaf8427b8c92ff58fd548c0
beveratraining <79100134+beveratraining@users.noreply.github.com>
2022-01-20 23:32:07 -0500

    Add files via upload

--- 10 -t test.R
+++ 10 -t test.R
@@ -4,7 +4,7 @@
 #Two vectors from the same sample group (i.e., paired t-test).
 
 #t.test(x, y = NULL,
-#       mu = 0, var.equal = FALSE)
+# mu = 0, var.equal = FALSE)
 #arguments:
 # - x : A vector to compute the one-sample t-test
 # - y: A second vector to compute the two sample t-test
@@ -25,3 +25,22 @@ t.test(smp2014, smp2019, var.equal=FALSE)
 
 #Run T-test of Equal Variance
 t.test(smp2014, smp2019, var.equal=TRUE)
+
+xhat1 <-mean(smp2014)
+xhat2 <-mean(smp2019)
+
+s1<-sd(smp2014)
+s2<-sd(smp2019)
+
+n1<-length(smp2014)
+n2<-length(smp2019)
+
+diff_mean<- xhat1-xhat2
+
+SE_diff_mean <- sqrt(s1^2/n1 + s2^2/n2)
+
+t_stat<-diff_mean/SE_diff_mean
+t_stat
+
+pvalue = 2*pt(t_stat,df=n1+n2-2)
+pvalue
